933354cd3200dd769fa73b0ee9a4138828c67a44
Cecilie <cepe0001@stud.kea.dk>
2024-11-04 12:20:56 +0100

    fjernet pages mappen

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
-import Image from "next/image";
 import MemberList from "@/components/MemberList";
 import Header from "@/components/Header";
-import SignuoForm from "@/components/SignupForm";
+import SignupForm from "@/components/SignupForm";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <div className="mx-auto max-w-lg px-4 py-6">
         <Header />
-        <SignuoForm />
+        <SignupForm />
         <MemberList />
       </div>
     </div>
